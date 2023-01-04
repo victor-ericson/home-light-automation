9e470c9af81be87acfcd17b029a821108c5b1347
filip40 <120582936+filip40@users.noreply.github.com>
2023-01-04 12:06:25 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -6,9 +6,7 @@ from mycroft import MycroftSkill, intent_file_handler
 livingroom = 17  # physical pin 11
 bedroom = 18  # physical pin 12
 kitchen = 19  # physical pin 35
-pwm_bedroom_get_duty_cycle = 0
-pwm_livingroom_get_duty_cycle = 0
-pwm_kitchen_get_duty_cycle = 0
+
 
 class Homelightautomation(MycroftSkill):
     def __init__(self):
@@ -16,7 +14,9 @@ class Homelightautomation(MycroftSkill):
         global pwm_bedroom
         global pwm_livingroom
         global pwm_kitchen
-
+        global pwm_bedroom_get_duty_cycle
+        global pwm_livingroom_get_duty_cycle
+        global pwm_kitchen_get_duty_cycle
         GPIO.setwarnings(False)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(livingroom, GPIO.OUT)
@@ -28,6 +28,9 @@ class Homelightautomation(MycroftSkill):
         pwm_bedroom.start(0)
         pwm_livingroom.start(0)
         pwm_kitchen.start(0)
+        pwm_bedroom_get_duty_cycle = 0
+        pwm_livingroom_get_duty_cycle = 0
+        pwm_kitchen_get_duty_cycle = 0
 
 
 
